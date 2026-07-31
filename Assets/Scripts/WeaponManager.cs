8c589463c294e78211426defa1721180681bee20
Tapyst <tapyst0@gmail.com>
2026-07-31 01:34:55 -0700

    Added Gun Logic Start 1.0

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
-    //private List<Weapon> weapons = new List<Weapon>();
-
+    
+    [SerializeField] public List<Weapon> weaponPrefabs = new List<Weapon>();
+    private List<GameObject> weaponGameObjects = new List<GameObject>();
+    public GameObject weaponPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 6; i++)
+        {
+            AddWeapon(RandomWeapon());
+        }
+        for (int i = 0; i < GameData.weapons.Count; i++)
+        {
+            Weapon weapon = GameData.weapons[i];
+            float angle = (360 / GameData.weapons.Count) * i;
+            weaponGameObjects.Add(Instantiate(weaponPrefab, new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle) * 2, Mathf.Sin(Mathf.Deg2Rad * angle) * 2, 0), Quaternion.identity, transform));
+            weaponGameObjects[i].GetComponent<SpriteRenderer>().sprite = weapon.sprite;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void AddWeapon(Weapon weapon)
     {
-        
+        GameData.weapons.Add(weapon);
+    }
+    private Weapon RandomWeapon()
+    {
+        int randomIndex = Random.Range(0, weaponPrefabs.Count);
+        return weaponPrefabs[randomIndex];
     }
 }
--- Assets/Scripts/WeaponPointer.cs
+++ Assets/Scripts/WeaponPointer.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class WeaponPointer : MonoBehaviour
 {   
-    public GameObject player;
+    private GameObject player;
     // Update is called once per frame
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) {
+            Debug.LogError("CameraFollow: No object with tag 'Player' found!");
+            return;
+        }
+    }
     void Update()
     {
         Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - new Vector3(0f, 0f, Camera.main.transform.position.z) - transform.position;
